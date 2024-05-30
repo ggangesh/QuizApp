2acc2f31757e1da11e53f15b474af95e3fde8082
gangesh <gangesh@.com>
2024-05-30 22:58:12 +0530

    App Welcome and Admin Operations display

--- src/com/quizapp/display/AdminDisplay.java
+++ src/com/quizapp/display/AdminDisplay.java
@@ -1,4 +1,41 @@
 package com.quizapp.display;
 
+import java.util.Scanner;
+
 public class AdminDisplay {
+    Scanner scanner = new Scanner(System.in);
+
+    public void allOperations(String msg) {
+        if (msg == null) {
+            System.out.println("Hi Admin, You may perform these operations");
+        } else {
+            System.out.println(msg);
+        }
+        System.out.println(" 1. Get all students scores");
+        System.out.println(" 2. Get particular student score");
+        System.out.println(" 3. Add question for quiz");
+        System.out.println(" 0. OR Go to App Home");
+        int choice = scanner.nextInt();
+        switch (choice) {
+            case 1:
+//                displayAllStudentsScoreInAscOrder();
+                break;
+            case 2:
+                displayStudentScoreById();
+                break;
+            case 3:
+//                addQuestion();
+        }
+        System.out.println("Do you want to do other operations? (Y/N)");
+        String ifFutherOps = scanner.next();
+        if (ifFutherOps.equalsIgnoreCase("Y")) {
+            allOperations("");
+        }
+    }
+
+    private void displayStudentScoreById() {
+        System.out.println("Enter student ID");
+        int studentID = scanner.nextInt();
+        getStudentScoreById();
+    }
 }
--- src/com/quizapp/display/AppDisplay.java
+++ src/com/quizapp/display/AppDisplay.java
@@ -1,4 +1,23 @@
 package com.quizapp.display;
 
+import java.util.Scanner;
+
 public class AppDisplay {
+    Scanner scanner = new Scanner(System.in);
+
+    public void welcome() {
+        System.out.println("Welcome to QuizApp");
+        System.out.println("How would you like to visit as ?");
+        System.out.println("1. Student  or else as ");
+        System.out.println("2. Admin?");
+        int role = scanner.nextInt();
+        switch (role) {
+            case 1:
+                break;
+            case 2:
+                AdminDisplay admin = new AdminDisplay();
+                admin.allOperations(null);
+                break;
+        }
+    }
 }
