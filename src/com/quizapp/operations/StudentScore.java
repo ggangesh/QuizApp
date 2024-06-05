b92fb4373fb41ab46acd8652f58ac1a7222f2d7d
gangesh <gangesh@.com>
2024-06-05 11:42:13 +0530

    formatted code and fixed questions not in quiz error

--- src/com/quizapp/operations/StudentScore.java
+++ src/com/quizapp/operations/StudentScore.java
@@ -1,25 +1,25 @@
 package com.quizapp.operations;
 
 public class StudentScore {
-	private String firstName;
-	private String lastName;
-	private int score;
+    private String firstName;
+    private String lastName;
+    private int score;
 
-	public StudentScore(String firstName, String lastName, int score) {
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.score = score;
-	}
+    public StudentScore(String firstName, String lastName, int score) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.score = score;
+    }
 
-	public String getFirstName() {
-		return firstName;
-	}
+    public String getFirstName() {
+        return firstName;
+    }
 
-	public String getLastName() {
-		return lastName;
-	}
+    public String getLastName() {
+        return lastName;
+    }
 
-	public int getScore() {
-		return score;
-	}
+    public int getScore() {
+        return score;
+    }
 }
